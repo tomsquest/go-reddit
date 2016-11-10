cfce138222727e0e66e2f89d251d27706ea91cb9
Thomas Queste <tom@tomsquest.com>
2016-11-10 17:59:41 +0100

    Can fake reddit with a flag

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/tomsquest/go-reddit/output"
 	"github.com/tomsquest/go-reddit/reddit"
 	"log"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 //go:generate go-bindata -nomemcopy -o assets/assets.go -pkg assets assets
@@ -12,19 +15,25 @@ import (
 func main() {
 	subredditNameParam := flag.String("subreddit", "golang", "Subreddit to fetch")
 	outputParam := flag.String("output", "console", "Select the output: console, mail")
+	fakeRedditParam := flag.Bool("fakeReddit", false, "Use predefined posts instead of calling Reddit")
 	flag.Parse()
 
 	client := reddit.NewClient()
 
-	subreddit, err := client.GetTopPosts(*subredditNameParam)
-	if err != nil {
-		log.Fatalf("Unable to get posts of subreddit %v: %v", subredditNameParam, err)
+	var subreddit reddit.Subreddit
+	if *fakeRedditParam {
+		posts := []reddit.Post{}
+		for i := 0; i < 10; i++ {
+			posts = append(posts, reddit.Post{Title: "Title " + strconv.Itoa(i), Url: "http://lorempixel.com/400/400/", Thumbnail: "http://lorempixel.com/400/400/", Created: reddit.PostTime{Time: time.Now()}, Ups: rand.Int(), NumComments: rand.Int()})
+		}
+		subreddit = reddit.NewSubreddit(*subredditNameParam, time.Now(), posts)
+	} else {
+		sub, err := client.GetTopPosts(*subredditNameParam)
+		subreddit = sub
+		if err != nil {
+			log.Fatalf("Unable to get posts of subreddit %v: %v", subredditNameParam, err)
+		}
 	}
-
-	//subreddit := reddit.NewSubreddit(*subredditNameParam, time.Now(), []reddit.Post{
-	//	{Title:"Title 1"},
-	//	{Title:"Title 2"},
-	//})
 
 	var selectedOutput output.Output
 	switch *outputParam {
