7d1dd7ed6458c164f1f66ccf19a3d9807c829fb9
Thomas Queste <tom@tomsquest.com>
2016-11-14 17:13:14 +0100

    Add log info for mail sending

--- output/smtp.go
+++ output/smtp.go
@@ -1,6 +1,7 @@
 package output
 
 import (
+	"github.com/mgutz/logxi/v1"
 	"github.com/tomsquest/go-reddit/assets"
 	"github.com/tomsquest/go-reddit/reddit"
 	"gopkg.in/gomail.v2"
@@ -19,20 +20,22 @@ func (SmtpOutput) Out(subreddit reddit.Subreddit) error {
 		return err
 	}
 
-	m := gomail.NewMessage()
-	m.SetHeader("From", "tom@tomsquest.com")
-	m.SetHeader("To", "tom@tomsquest.com")
-	m.SetHeader("Subject", "Subreddit "+strings.ToUpper(subreddit.Name)+" - "+toDate(subreddit.CrawlDate))
-	m.SetBody("text/html", html)
-	m.Embed("reddit_logo.png", gomail.SetCopyFunc(func(w io.Writer) error {
+	mail := gomail.NewMessage()
+	mail.SetHeader("From", "tom@tomsquest.com")
+	mail.SetHeader("To", "tom@tomsquest.com")
+	mail.SetHeader("Subject", "Subreddit "+strings.ToUpper(subreddit.Name)+" - "+toDate(subreddit.CrawlDate))
+	mail.SetBody("text/html", html)
+	mail.Embed("reddit_logo.png", gomail.SetCopyFunc(func(w io.Writer) error {
 		data, err := assets.Asset("assets/reddit_logo.png")
 		w.Write(data)
 		return err
 	}))
 
+	log.Info("Sending email", "subject", mail.GetHeader("Subject"))
+
 	host := os.Getenv("SMTP_HOST")
 	port, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
 	user := os.Getenv("SMTP_USER")
 	pass := os.Getenv("SMTP_PASS")
-	return gomail.NewDialer(host, port, user, pass).DialAndSend(m)
+	return gomail.NewDialer(host, port, user, pass).DialAndSend(mail)
 }
