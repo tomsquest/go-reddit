8346207d4bd88a1826cd92811433e3b441202259
Thomas Queste <tom@tomsquest.com>
2016-11-14 10:59:53 +0100

    Rand Ups/Comment between 0 and 100

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	if *fakeRedditParam {
 		posts := []reddit.Post{}
 		for i := 0; i < 10; i++ {
-			posts = append(posts, reddit.Post{Title: "Title " + strconv.Itoa(i), Url: "http://lorempixel.com/400/400/", Thumbnail: "http://lorempixel.com/400/400/", Created: reddit.PostTime{Time: time.Now()}, Ups: rand.Int(), NumComments: rand.Int()})
+			posts = append(posts, reddit.Post{Title: "Title " + strconv.Itoa(i), Url: "http://lorempixel.com/400/400/", Thumbnail: "http://lorempixel.com/400/400/", Created: reddit.PostTime{Time: time.Now()}, Ups: rand.Intn(1000), NumComments: rand.Intn(1000)})
 		}
 		subreddit = reddit.NewSubreddit(*subredditNameParam, time.Now(), posts)
 	} else {
